d8c072d6f98cceed9b74c5bba7d2424c4e631121
Prelang Builder <builder@prelang.com>
2015-06-24 19:00:55 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'CompanyInfo' to allow for create, update

--- app/admin/company_info.rb
+++ app/admin/company_info.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register CompanyInfo do
+  permit_params :name, :contract_date, :liability_insurance_date, :liability_insurance_company, :liability_insurance_policy_number, :workers_comp_company, :workers_comp_policy_number, :business_registrataion_expiration, :annual_filing_date
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
