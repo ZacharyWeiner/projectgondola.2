44a1600aa268f87482af17df730c9ca328338c24
Prelang Builder <builder@prelang.com>
2015-06-24 18:59:01 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'WorkOrderTime' to allow for create, update

--- app/admin/work_order_time.rb
+++ app/admin/work_order_time.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register WorkOrderTime do
+  permit_params :esitmated_job_duration, :estimated_travel_time, :estimated_wrap_up_duration, :estimated_start, :estimated_wrapup, :actual_job_duration, :actual_travel_duration, :actual_wrap_up_duration, :actual_end_time, :actual_start_time, :actual_completed_time, :standard_job_duration
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
