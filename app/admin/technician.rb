e36f76e84385aae6fdc3736c1fab24a16b32774e
Prelang Builder <builder@prelang.com>
2015-06-24 19:01:08 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Technician' to allow for create, update

--- app/admin/technician.rb
+++ app/admin/technician.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Technician do
+  permit_params :mso_tech_id, :last_background_check, :gchat_id, :is_active, :home_phone, :cell_phone, :last_drug_test, :internal_tech_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
