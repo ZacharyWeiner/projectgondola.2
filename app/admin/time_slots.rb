3813d40c46a225fef6947c49b9691f48ebafa2c2
Prelang Builder <builder@prelang.com>
2015-06-24 18:59:38 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'TimeSlots' to allow for create, update

--- app/admin/time_slots.rb
+++ app/admin/time_slots.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register TimeSlots do
+  permit_params :code, :description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
