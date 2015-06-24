9c3cc02aea15e248da125a6c976287144e13de85
Prelang Builder <builder@prelang.com>
2015-06-24 19:00:26 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'WorkOrder' to allow for create, update

--- app/admin/work_order.rb
+++ app/admin/work_order.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register WorkOrder do
+  permit_params :work_order_uid, :work_order_number, :completed, :slot_start, :slot_end, :priority, :tech_login_id, :previous_fsr_login_id, :job_id, :dwelling_type, :job_type, :job_class, :requirements_list, :privilege_list, :routing_area_code, :routing_area, :account_number, :first_name, :last_name, :home_phone, :call_ahead_number, :customer_id, :cod_amount, :drop_type, :processing_code, :service_code, :equipment_type, :equipment_serial_number
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
